58b98d250bc38dd113e4ae043c10df9b1316e225
Markus Iversflaten <markus.h.iversflaten@jpl.nasa.gov>
2024-03-28 15:48:46 -0700

    Add BaseController yaml file reading

--- smallsat_sim/controllers/base_controller.py
+++ smallsat_sim/controllers/base_controller.py
@@ -1,3 +1,8 @@
+import yaml
+import os
+
+from smallsat_sim import SMALLSAT_SIM_CONTROLLERS_DIR
+
 class BaseController(object):
     def __init__(self) -> None:
         pass
@@ -6,3 +11,19 @@ class BaseController(object):
         """
         Defines the controller callback for the simulation step.
         """
+
+    def _load_cfg(self, controller_name: str) -> dict:
+        """
+        Loads the config parameters from the file located in cfg/config.yaml    
+        """
+        # localize relevant yaml file
+        cfg_path = os.path.join(SMALLSAT_SIM_CONTROLLERS_DIR, 
+                                controller_name, "cfg", "config.yaml")
+
+        # load from yaml file
+        with open(cfg_path) as file:
+            try:
+                cfg = yaml.safe_load(file)
+                return cfg
+            except yaml.YAMLError as exc:
+                print(exc)
