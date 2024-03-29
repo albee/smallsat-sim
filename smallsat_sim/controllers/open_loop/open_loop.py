78f69f7e86819fc9f23009627d4cd20b5c2f9099
Alexander Hansson <alexander.gustav.hansson@jpl.nasa.gov>
2024-03-28 17:19:19 -0700

    Open-loop example

--- smallsat_sim/controllers/open_loop/open_loop.py
+++ smallsat_sim/controllers/open_loop/open_loop.py
@@ -1,7 +1,18 @@
 from smallsat_sim.controllers.base_controller import BaseController
 
+import numpy as np
+import mujoco
+
 class OpenLoop(BaseController):
     def __init__(self) -> None:
         super().__init__()
+        
+        # register controller callback in mujoco
+        mujoco.set_mjcb_control(self.controller_callback)
 
-        # Load specifc parameters from cfg folder and assign to self
+    def controller_callback(self, model, data) -> None:
+        force0 = np.cos(data.time)
+        force1 = np.cos(data.time + np.pi)
+
+        data.ctrl[0] = force0
+        data.ctrl[1] = force1
