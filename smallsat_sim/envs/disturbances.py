03f240519f38824f35cfc0d8fe5f10dd39ff6298
Alexander Hansson <alexander.gustav.hansson@jpl.nasa.gov>
2024-04-04 14:31:04 -0700

    Implemented noise generator
    Base class + Simple example

--- smallsat_sim/envs/disturbances.py
+++ smallsat_sim/envs/disturbances.py
@@ -1,5 +1,4 @@
-
-
+import numpy as np
 
 class Disturbance(object):
     """
@@ -12,3 +11,42 @@ class Disturbance(object):
 
     def apply(self):
         pass
+
+class DisturbanceList(Disturbance):
+    """
+    Applies multiple disturbances sequentially to a given input.
+    """
+    def __init__(self,
+                 disturbances: list[Disturbance]
+                 ) -> None:
+        super().__init__()
+        self.disturbances = disturbances
+
+    def apply(self):
+        pass
+
+class ConstantForceDisturbance(Disturbance):
+    """
+    Applies a constant force disturbance in a specified direction
+    """ 
+    def __init__(self,
+                 magnitude: float,
+                 direction: np.ndarray
+                 ) -> None:
+        super().__init__()
+
+        # Check dimensions
+        if not np.isscalar(magnitude):
+            raise ValueError("Magnitude must be a scalar")
+        if direction.shape != (3,):
+            raise ValueError("Direction must have shape (3,1)")
+
+        self.magnitude = magnitude
+        self.direction = direction / np.linalg.norm(direction)
+        
+        # Save constant disturbance as 6D array
+        force, torque = self.magnitude * self.direction, np.array([0, 0, 0])
+        self.disturbance = np.concatenate((force, torque))
+
+    def apply(self) -> np.ndarray:
+        return self.disturbance
