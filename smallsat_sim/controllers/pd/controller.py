8b04972d7e8a4a6737cddd461f163e1dd7140c6b
Markus Iversflaten <markus.h.iversflaten@jpl.nasa.gov>
2024-04-09 16:06:01 -0700

    Tweak of license

--- smallsat_sim/controllers/pd/controller.py
+++ smallsat_sim/controllers/pd/controller.py
@@ -2,11 +2,9 @@
 # Copyright (c) 2017, United States Government, as represented by the
 # Administrator of the National Aeronautics and Space Administration.
 # 
-# All rights reserved.
-# 
-# The Astrobee platform is licensed under the Apache License, Version 2.0
-# (the "License"); you may not use this file except in compliance with the
-# License. You may obtain a copy of the License at
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 # 
 #     http://www.apache.org/licenses/LICENSE-2.0
 # 
