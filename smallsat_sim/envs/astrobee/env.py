98d5fc807b7262273f6eb9d6f1c69b2b2cd2e122
Alexander Hansson <alexander.gustav.hansson@jpl.nasa.gov>
2024-04-14 11:24:21 -0700

    Fix PD experiment

--- smallsat_sim/envs/astrobee/env.py
+++ smallsat_sim/envs/astrobee/env.py
@@ -6,7 +6,9 @@ import os
 
 class AstrobeeEnv(BaseEnv):
     def __init__(self,args) -> None:
-        self.cfg = self._load_cfg("astrobee")
+        # Load necessary config files
+        self.env_cfg, self.lib_cfg = self._load_cfg(env_name="astrobee",
+                                                    lib_name="astrobee")
         super().__init__(args=args)
 
     def _init_mujoco(self) -> None:
@@ -15,12 +17,3 @@ class AstrobeeEnv(BaseEnv):
         self.model = mujoco.MjModel.from_xml_path(xml)
         self.data = mujoco.MjData(self.model)
         self._create_viewer()
-
-    def step(self) -> None:
-        # Step simulation
-        # Controller callback is called internally to retrieve inputs
-        # See: https://mujoco.readthedocs.io/en/latest/APIreference/APIglobals.html#mjcb-control
-        mujoco.mj_step(self.model,self.data)
-
-        # Update renderer
-        self.viewer.sync()
