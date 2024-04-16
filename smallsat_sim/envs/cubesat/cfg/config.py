05802ba2543f7ae7b5713ae5245cdcc7fc39ad79
Markus Iversflaten <markus.h.iversflaten@jpl.nasa.gov>
2024-04-16 11:42:07 -0700

    Change formatting for MJCF

--- smallsat_sim/envs/cubesat/cfg/config.py
+++ smallsat_sim/envs/cubesat/cfg/config.py
@@ -8,11 +8,12 @@ class Body():
 class EnvConfig:
     def __init__(self,args) -> None:
         self.model = 'cubesat'
-        self.compiler = {'convexhull': 'false'}
+        self.compiler = {'convexhull': 'true'}
         self.visual = {'headlight': {'ambient': '0.5 0.5 0.5', 'specular': '0.5 0.5 0.5', 'diffuse': '0.5 0.5 0.5'}}
         self.num_bodies = args.num_bodies
     class Bodies:
-        def __init__(self) -> None:
+        def __init__(self,num_bodies) -> None:
+            self.num_bodies = num_bodies
             self.bodies_list = []
             for i in range(self.num_bodies):
                 self.bodies_list.append(Body(name=f'body{i}',pos=f"{i} 0 10"))
