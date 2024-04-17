9ab73a9e521ed3807ae76350184ecbbbc6e10c36
Markus Iversflaten <markus.h.iversflaten@jpl.nasa.gov>
2024-04-17 16:49:19 -0700

    Create CubeSat environment using env and lib configs

---
+++ experiments/cubesat_PD.py
@@ -0,0 +1,34 @@
+import time
+from smallsat_sim.utils.helpers import get_args
+from smallsat_sim.controllers.pd.controller import PDController
+from smallsat_sim.envs.cubesat.env import CubesatEnv
+from smallsat_sim.planners.oracle.oracle import OraclePlanner
+
+# Get arguments for script execution
+args = get_args()
+
+# Create environment
+env = CubesatEnv(args=args)
+
+# Create planner
+planner = OraclePlanner(env)
+
+# Create controller
+ctrl = PDController(env, planner)
+
+# Define start time
+start_time = time.time()
+
+# Simulation loop
+while True:
+
+    real_time = time.time() - start_time
+
+    sim_time = env.data.time
+
+    if sim_time < real_time:
+        # Calculate control action (open-loop)
+        ctrl_input = ctrl.get_control_input(env)
+
+        # Advance simulation
+        env.step(input=ctrl_input)
