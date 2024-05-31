a0c4794348ed525e59d2e4c2e9502dfc84e6b059
David Schwartz <david.schwartz@jpl.nasa.gov>
2024-05-30 17:23:12 -0700

    Bug fixes and refactoring

--- smallsat_sim/controllers/lqr/controller.py
+++ smallsat_sim/controllers/lqr/controller.py
@@ -3,6 +3,7 @@ from smallsat_sim.envs.base_env import BaseEnv
 
 import numpy as np
 import casadi as ca
+import control
 
 from scipy.linalg import solve_discrete_are, solve_continuous_are, expm
 
@@ -27,13 +28,31 @@ class LQRController(BaseController):
         else:
             self._visualize = lambda *args, **kwargs: None
 
-        # Compute the steady-state LQR gain
-        self.K = self.get_ss_lqr_gain(env)
+        # Set reference values for parts of the states
+        self.quat_ref = np.array([1.0, 0.0, 0.0, 0.0]).reshape(4,)
+        self.v_ref = np.zeros((3, 1)).reshape(3,)
+        self.omega_ref = np.zeros((3, 1)).reshape(3,)
 
+        # Get the cost function matrices
+        nx = self.ctrl_cfg.cost.Q.shape[0]
+        self.Q = self.ctrl_cfg.cost.Q + 1e-5 * np.eye(nx, nx) # Perturb for numerical stability
+        self.R = self.ctrl_cfg.cost.R
 
-    def get_ss_lqr_gain(self, env: BaseEnv) -> np.ndarray:
+    
+    def check_controllability(self, A, B) -> bool:
         """
-        Calculate the LQR gain around a steady-state.
+        Check if the system is controllable.
+        """
+        ctrb_matrix = control.ctrb(A, B)
+        if (np.linalg.matrix_rank(ctrb_matrix) != A.shape[0]):
+            print("System is not controllable.")
+            return False
+        return True
+
+
+    def get_lqr_gain(self, env: BaseEnv) -> np.ndarray:
+        """
+        Calculate the LQR gain.
         """
 
         # Fetch symbolic model
@@ -41,15 +60,12 @@ class LQRController(BaseController):
         f = model.f_expl_expr
         x = model.x
         u = model.u
+        nx = x.shape[0]
+        nu = u.shape[0]
 
         # Define the operating point
-        x_s = ca.SX.zeros(model.x.shape[0], 1)
-        x_s[3] = 1.0
-        x_s[10] = 0.5
-        x_s[11] = 0.6
-        x_s[12] = 0.2
-        x_s = ca.SX(env.obs[0:13])
-        u_s = ca.SX.zeros(model.u.shape[0], 1)
+        x_s = ca.SX(env.obs[0:nx])
+        u_s = ca.SX.zeros(model.nu, 1)
 
         # Linearize the model around the operating point
         A_sym = ca.jacobian(f, x)
@@ -58,28 +74,27 @@ class LQRController(BaseController):
         B_num = ca.substitute(B_sym, u, u_s)
 
         # Convert the A and B matrices to NumPy
-        A = np.zeros((A_num.shape[0], A_num.shape[1]))
-        for i in range(A_num.shape[0]):
-            for j in range(A_num.shape[1]):
+        A = np.zeros((nx, nx))
+        for i in range(nx):
+            for j in range(nx):
                 A[i, j] = A_num[i, j]
-        B = np.zeros((B_num.shape[0], B_num.shape[1]))
-        for i in range(B_num.shape[0]):
-            for j in range(B_num.shape[1]):
+        B = np.zeros((nx, nu))
+        for i in range(nx):
+            for j in range(nu):
                 B[i, j] = B_num[i, j]
 
-        # Get the cost function matrices
-        Q = self.ctrl_cfg.cost.Q
-        R = self.ctrl_cfg.cost.R
-
-        # Discretize the system
-        A = np.eye(A.shape[0]) + A * 0.5
+        # Discretize the system (Euler discretization)
+        A = np.eye(nx) + A * 0.5
         B = B * 0.5
 
+        # Check if the system is controllable (for debugging purposes)
+        # is_controllable = self.check_controllability(A, B)
+
         # Solve the DARE
-        P = solve_discrete_are(A, B, Q, R)
+        P = solve_discrete_are(A, B, self.Q, self.R)
 
         # Compute the LQR gain
-        K = np.linalg.inv(B.T @ P @ B + R) @ (B.T @ P @ A)
+        K = np.linalg.inv(B.T @ P @ B + self.R) @ (B.T @ P @ A)
 
         return K
 
@@ -92,7 +107,14 @@ class LQRController(BaseController):
         # Get current state
         x = env.obs[0:13]
 
-        # Compute optimal control signal
-        uopt = - self.K @ x # Penalize the diff. with x_s instead
+        # Compute the LQR gain
+        self.K = self.get_lqr_gain(env)
+        
+        # Get the reference position
+        r_ref = self.planner.get_reference(env.obs).reshape(3,)
+        x_ref = np.concatenate((r_ref, self.quat_ref, self.v_ref, self.omega_ref))
 
-        return uopt
+        # Compute optimal control signal
+        u_opt = - self.K @ (x - x_ref)
+
+        return u_opt
