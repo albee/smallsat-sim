0b387a48a761d371423a906607bc0c898406acbd
David Schwartz <david.schwartz@jpl.nasa.gov>
2024-06-18 17:04:23 -0700

    Set up RL skeleton

---
+++ smallsat_sim/controllers/rl/controller.py
@@ -0,0 +1,10 @@
+from smallsat_sim.controllers.base_controller import BaseController
+from smallsat_sim.envs.base_env import BaseEnv
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+class RLController(BaseController):
+    pass
