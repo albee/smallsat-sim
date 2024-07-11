789e29ad891928249fc2c3501153222617c45d4a
Alexander Hansson <alexander.gustav.hansson@jpl.nasa.gov>
2024-07-10 17:50:27 -0700

    Define initial 2 waypoints

--- smallsat_sim/planners/mission/mission.py
+++ smallsat_sim/planners/mission/mission.py
@@ -1,5 +1,8 @@
 from smallsat_sim.planners.base_planner import BasePlanner
 
+import numpy as np
+
+
 class MissionPlanner(BasePlanner):
     def __init__(self, env) -> None:
         super().__init__(env)
@@ -12,8 +15,20 @@ class MissionPlanner(BasePlanner):
         Loads the sparse waypoints that shall be reached
         """
         # Sets list of points
-        self.waypoints = []
+        self.waypoints = [
+            [0, 0, 10],
+            [0, 10, 0],
+            [10, 0, 0],
+            [-3.4, -9, 0], # Point 1
+            [-3.4, -25, 0], # Point 1
+        ]
 
         # Visualize wapoints for DEBUG
         if True:
-            self.visualize(self.waypoints)
+            self.visualize(self.waypoints, size=[0.2, 0, 0])
+
+    def get_reference(self, obs: np.ndarray) -> np.ndarray:
+        """
+        Returns a reference point based on current observations
+        """
+        return np.array([0, 0, 10])
