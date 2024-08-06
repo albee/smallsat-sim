64fd3b23de7307d8962956685c861995f0f2a9bc
Alexander Hansson <alexander.gustav.hansson@jpl.nasa.gov>
2024-08-06 13:41:07 -0700

    Sliding window inherits from OnlineLearningStrategy

--- smallsat_sim/controllers/gp_mpc/online_learning/strategies.py
+++ smallsat_sim/controllers/gp_mpc/online_learning/strategies.py
@@ -1,3 +1,4 @@
+from gpytorch.models.exact_gp import ExactGP
 from numpy.core.multiarray import array as array
 from zero_order_gpmpc.models.gpytorch_models.gpytorch_residual_learning_model import (
     DataProcessingStrategy,
@@ -10,14 +11,25 @@ from zero_order_gpmpc.models.gpytorch_models.gpytorch_residual_learning_model im
 
 import numpy as np
 import torch
+from zero_order_gpmpc.models.gpytorch_models.gpytorch_residual_model import (
+    FeatureSelector,
+)
 
 
-class SlidingWindow(DataProcessingStrategy):
+class SlidingWindow(OnlineLearningStrategy):
     """
-    Implements the sliding window approach to adapt the points
-    used for posterior predictions
+    This Dataprocessing strategy updates data by using a sliding window over
+    past observations, keeping the most recent ones and discarding older data
     """
 
-    raise NotImplementedError(
-        "The SlidingWindow approach has not been implemented yet."
-    )
+    def __init__(self, max_num_points: int = 200, device: str = "cpu") -> None:
+        super().__init__(max_num_points, device)
+
+    def process(
+        self,
+        gp_model: ExactGP,
+        x_input: np.array,
+        y_target: np.array,
+        gp_feature_selector: FeatureSelector,
+    ) -> ExactGP | None:
+        pass
