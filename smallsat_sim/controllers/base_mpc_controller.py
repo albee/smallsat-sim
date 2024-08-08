f48e4422474d5de2239cd5469a913926e6b7cd58
Alexander Hansson <alexander.gustav.hansson@jpl.nasa.gov>
2024-08-07 17:14:58 -0700

    Add base class for all MPC controllers

---
+++ smallsat_sim/controllers/base_mpc_controller.py
@@ -0,0 +1,32 @@
+from smallsat_sim.envs.base_env import BaseEnv
+from smallsat_sim.planners.base_planner import BasePlanner
+from smallsat_sim.controllers.base_controller import BaseController
+
+from abc import abstractmethod
+
+
+class BaseMPCController(BaseController):
+    def __init__(self, env: BaseEnv, planner: BasePlanner, ctrl_cfg: object) -> None:
+
+        super().__init__(env, planner, ctrl_cfg)
+
+    @abstractmethod
+    def get_control_input(self, env: BaseEnv) -> None:
+        """
+        Returns the control input
+        """
+        pass
+
+    @abstractmethod
+    def _visualize_prediction(self) -> None:
+        """
+        Plot predicted trajectory of MPC in MuJoCo viewer.
+        """
+        pass
+
+    @abstractmethod
+    def _visualize_prediction_renderer(self) -> None:
+        """
+        Plot predicted trajectory of MPC in MuJoCo renderer.
+        """
+        pass
