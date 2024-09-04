8a1b133fcbf00b58e95e336a4524358f077042f9
Alexander Hansson <alexander.gustav.hansson@jpl.nasa.gov>
2024-09-04 09:51:51 -0700

    Implement sliding window plus strategy

--- smallsat_sim/controllers/gp_mpc/online_learning/strategies.py
+++ smallsat_sim/controllers/gp_mpc/online_learning/strategies.py
@@ -13,6 +13,7 @@ from zero_order_gpmpc.models.gpytorch_models.gpytorch_utils import to_tensor, to
 import numpy as np
 import torch
 import gpytorch
+from typing import Optional
 from zero_order_gpmpc.models.gpytorch_models.gpytorch_residual_model import (
     FeatureSelector,
 )
@@ -20,6 +21,9 @@ from zero_order_gpmpc.models.gpytorch_models.gpytorch_residual_model import (
 from smallsat_sim.external.zero_order_gp_mpc_package.external.gpytorch_utils.gp_hyperparam_training import (
     train_gp_model,
 )
+
+from smallsat_sim.controllers.gp_mpc.online_learning.utils import ResidualScaler
+from scipy.stats import norm
 
 
 class SlidingWindow(OnlineLearningStrategy):
@@ -144,3 +148,149 @@ class SlidingWindow(OnlineLearningStrategy):
             return fantasy_model
         else:
             return fantasy_model
+
+
+class SlidingWindowPlus(OnlineLearningStrategy):
+    def __init__(self, max_num_points: int = 200, device: str = "cpu") -> None:
+        # Initialize superclass
+        super().__init__(max_num_points, device)
+
+        # Initialize SlidingWindow-specific variables
+        self.timestamps = []
+
+        # How many times called process
+        self.counter = -1
+
+    def process(
+        self,
+        gp_model: ExactGP,
+        x_input: np.array,
+        y_target: np.array,
+        gp_feature_selector: FeatureSelector,
+        timestamp: float,
+    ) -> ExactGP | None:
+
+        self.counter += 1
+
+        # Convert to tensor
+        if not torch.is_tensor(x_input):
+            x_input = to_tensor(arr=x_input, device=self.device)
+
+        if not torch.is_tensor(y_target):
+            y_target = to_tensor(arr=y_target, device=self.device)
+
+        # Extend to 2D for further computation
+        x_input = torch.atleast_2d(x_input)
+        y_target = torch.atleast_2d(y_target)
+
+        if (
+            gp_model.prediction_strategy is None
+            or gp_model.train_inputs is None
+            or gp_model.train_targets is None
+        ):
+            if gp_model.train_inputs is not None:
+                raise RuntimeError(
+                    "train_inputs in GP is not None. Something went wrong."
+                )
+
+            # Set the training data and return (in-place modification)
+            gp_model.set_train_data(
+                gp_feature_selector(x_input),
+                y_target,
+                strict=False,
+            )
+
+            # Record datapoint in timestamps
+            self.timestamps.append(timestamp)
+
+            return
+
+        # Check if the point shall be added to the dictionary
+        if not self._add_point_to_dict(
+            gp_model, x_input, y_target, gp_feature_selector, timestamp
+        ):
+            return gp_model
+
+        # Check if GP is already full
+        if gp_model.train_inputs[0].shape[-2] >= self.max_num_points:
+            with torch.no_grad():
+                # Initialize selector
+                selector = torch.ones(self.max_num_points, requires_grad=False)
+
+                # Find oldest datapoint to drop
+                drop_idx = self.timestamps.index(min(self.timestamps))
+                selector[drop_idx] = 0
+
+                # Calculate fantasy model with data selector
+                fantasy_model = gp_model.get_fantasy_model(
+                    gp_feature_selector(x_input),
+                    y_target,
+                    data_selector=selector,
+                )
+
+                # Update timestamps
+                self.timestamps.pop(drop_idx)
+                self.timestamps.append(timestamp)
+
+            # Check for training
+            fantasy_model = self._check_training(fantasy_model)
+
+            return fantasy_model
+
+        with torch.no_grad():
+            # Add observation and return updated model
+            fantasy_model = gp_model.get_fantasy_model(
+                gp_feature_selector(x_input), y_target
+            )
+
+            # Record datapoint in timestamps
+            self.timestamps.append(timestamp)
+
+            return fantasy_model
+
+    def _check_training(self, fantasy_model: ExactGP) -> ExactGP:
+        pass
+
+    def _add_point_to_dict(
+        self,
+        gp_model: gpytorch.models.ExactGP,
+        x_test: torch.Tensor,
+        y_test: torch.Tensor,
+        gp_feature_selector: FeatureSelector,
+        timestamp: float,
+    ) -> bool:
+        """
+        Checks all criteria if point shall be added to dictionary
+        """
+        # Calculate the expected mean at the location
+        with torch.no_grad():
+            z_test = torch.atleast_2d(gp_feature_selector(x_test))
+            observed_pred = gp_model(z_test)
+
+            # Extract mean
+            mu = observed_pred.mean
+            # print(mu)
+
+            # Extract Variance
+            stddev = observed_pred.stddev
+
+        # Calculate the beta quantity
+        beta = self._calc_beta(mu, y_test, stddev)
+
+        if beta.min() < 0.05:
+            print(f"Fault at {timestamp}!")
+
+        return True
+
+    def _calc_beta(self, mu: torch.Tensor, y_test: torch.Tensor, stddev: torch.Tensor):
+
+        # Calculate the standardized distance for each dimension
+        standardized_distance = torch.abs(mu - y_test) / stddev
+
+        # Calculate the argument for the normal CDF for each dimension
+        cdf_args = 1.96 - standardized_distance
+
+        # Calculate the probability using the CDF of the standard normal distribution
+        beta = norm.cdf(cdf_args.cpu().numpy())
+
+        return beta
