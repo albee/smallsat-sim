bd050a91b6dad450962c4b9bb05e7d9340cccd69
kalbee <kalbee@jpl.nasa.gov>
2024-10-02 11:06:33 -0700

    Add package identifiers and class diagram script.

---
+++ docs/generate_class_diagrams.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+# Dependency check
+check_for_package() {
+    PACKAGE_NAME=$1
+
+    # Check if the package is installed  
+    if pip show "$PACKAGE_NAME" &> /dev/null; then
+      echo "$PACKAGE_NAME is installed, continuing."
+    else
+      echo "$PACKAGE_NAME is not installed. Install with `pip install $PACKAGE_NAME`."
+    fi
+}
+check_for_package "pylint"
+
+# Generate useful class diagrams for a sim overview
+pyreverse -o png -p SmallSatSim ../smallsat_sim/
+pyreverse -o png -p Experiemnts ../experiments/
