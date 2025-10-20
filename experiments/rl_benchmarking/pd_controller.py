004cbed90520047aceaec261b1cf0d920175030b
David Schwartz <101265674+davidbschwartz@users.noreply.github.com>
2025-10-21 00:38:52 +0200

    Set XLA flags correctly

--- experiments/rl_benchmarking/pd_controller.py
+++ experiments/rl_benchmarking/pd_controller.py
@@ -1,11 +1,10 @@
 import os
 
+# Set flags to improve XLA performance on GPU
+os.environ["XLA_FLAGS"] = "--xla_gpu_triton_gemm_any=True "
+
 from experiments.rl_benchmarking.rl_benchmarker import Benchmarker
 
-# Set flags to improve XLA performance on GPU
-os.environ["XLA_FLAGS"] = (
-    "--xla_gpu_enable_triton_softmax_fusion=true " "--xla_gpu_triton_gemm_any=True "
-)
 
 benchmarker = Benchmarker()
 
