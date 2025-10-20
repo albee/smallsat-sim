46b12b30d66f240a2f20f9c88c6a93c7a1ad6f75
David Schwartz <101265674+davidbschwartz@users.noreply.github.com>
2025-10-21 00:48:39 +0200

    Fixed position of XLA flag

--- experiments/rl_training/train_astrobee.py
+++ experiments/rl_training/train_astrobee.py
@@ -1,18 +1,15 @@
-import time
 import os
+
+# Set flags to improve XLA performance on GPU
+os.environ["XLA_FLAGS"] = "--xla_gpu_triton_gemm_any=True "
+
+import time
 
 from smallsat_sim.utils.helpers import get_args
 from smallsat_sim.controllers.rl.runners.on_policy_runner import OnPolicyRunner
 from smallsat_sim.envs.astrobee_rl.env import AstrobeeEnvVectorized
-from smallsat_sim.planners.oracle.oracle import OraclePlanner
 from smallsat_sim.planners.oracle.oracle_rl import OraclePlannerRL
-from smallsat_sim.planners.mission.mission import MissionPlanner
 
-
-# Set flags to improve XLA performance on GPU
-os.environ["XLA_FLAGS"] = (
-    "--xla_gpu_enable_triton_softmax_fusion=true " "--xla_gpu_triton_gemm_any=True "
-)
 
 # Get arguments for script execution
 args = get_args()
