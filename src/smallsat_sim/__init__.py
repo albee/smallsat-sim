498b721413325bac63d56693d396347009f5d886
David Schwartz <101265674+davidbschwartz@users.noreply.github.com>
2025-10-29 21:53:54 +0100

    Limit memory that torch can block on GPU

--- src/smallsat_sim/__init__.py
+++ src/smallsat_sim/__init__.py
@@ -1,9 +1,18 @@
 import os
 
+try:
+    import torch
+except ImportError:
+    torch = None
+else:
+    if torch.cuda.is_available():
+        torch.cuda.set_per_process_memory_fraction(0.05, device=0)
+
+
 # Absolute path to root directoy to smallsat-sim folder
 SMALLSAT_SIM_ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
 
-# Absolute path to root directory of smallsat-steward repository
+# Absolute path to root directory of smallsat-steward repository
 SMALLSAT_STEWARD_ROOT_DIR = os.path.join(SMALLSAT_SIM_ROOT_DIR, "..")
 
 # Absolute path to controllers directory in smallsat-sim repository
